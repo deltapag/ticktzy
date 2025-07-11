37865b10fac74dd2680c6f205714698b0bfa18bd
Lucas Honda <lucashondastts@gmail.com>
2025-07-11 09:21:09 -0300

    Fix newline at EOF in config resources

--- app/src/main/java/br/com/sttsoft/ticktzy/presentation/config/ConfigActivity.kt
+++ app/src/main/java/br/com/sttsoft/ticktzy/presentation/config/ConfigActivity.kt
@@ -3,6 +3,7 @@ package br.com.sttsoft.ticktzy.presentation.config
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 import br.com.sttsoft.ticktzy.databinding.ActivityConfigBinding
 
 class ConfigActivity: AppCompatActivity() {
@@ -23,6 +24,46 @@ class ConfigActivity: AppCompatActivity() {
     fun setListeners() {
         binding.btnBack.setOnClickListener {
             finish()
+        }
+
+        binding.btnLayoutNewSale.setOnClickListener {
+            Toast.makeText(this, "Configurar Nova Venda", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutCharge.setOnClickListener {
+            Toast.makeText(this, "Configurar Cobrança", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutReports.setOnClickListener {
+            Toast.makeText(this, "Configurar Relatórios", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutSearchBar.setOnClickListener {
+            Toast.makeText(this, "Configurar SearchBar", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutPaymentBar.setOnClickListener {
+            Toast.makeText(this, "Configurar PaymentBar", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutProductList.setOnClickListener {
+            Toast.makeText(this, "Configurar Lista de Produtos", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutProductAdapter.setOnClickListener {
+            Toast.makeText(this, "Configurar ProductAdapter", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutProductView.setOnClickListener {
+            Toast.makeText(this, "Configurar ProductView", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutTerminal.setOnClickListener {
+            Toast.makeText(this, "Configurar Terminal", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayoutSitef.setOnClickListener {
+            Toast.makeText(this, "Configurar SITEF", Toast.LENGTH_SHORT).show()
         }
     }
 
